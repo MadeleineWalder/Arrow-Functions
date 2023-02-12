231661f666b336199d704001da71bb7b04f35ffe
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-02-12 11:58:49 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,42 @@
 
 // Vanilla JavaScript Function
 
+//function addTwoNumbers(a, b) {
+//return a + b;
+//}
+
+//let sum = addTwoNumbers(2, 2);
+//console.log(sum) 
+
 // Arrow Function With Parameters
+
+//const addTwoNumbers = (a, b) => {
+//    return a + b;
+//    }
+//    
+//    let sum = addTwoNumbers(2, 2);
+//    console.log(sum) 
 
 // Single Line Arrow Function With Parameters
 
+//function addTwoNumbers = (a, b) => a + b;
+//    
+//    let sum = addTwoNumbers(2, 2);
+//    console.log(sum) 
+
 // Implicit Returns
 
+//const saySomething = message => console.log(message);
+//saySomething('Hello there!');
+
+//const sayHello = () => console.log('hello');
+//sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+     This is a multiline string!
+    <p>`
+)
+console.log(returnMultipleLines());
